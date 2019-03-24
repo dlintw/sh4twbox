7ab333a9e3f5cbc116a37d3582293b9d5c1bb7d7
Daniel YC Lin <dlin.tw@gmail.com>
2019-03-24 19:36:46 +0800

    added a script to for setup testing environment

---
+++ tools/setup_network.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This is sample script for setup the following network
+#
+# Internet -> mobile phone -> usb share -> usb cable ->
+#   PC(on usb0) -> PC(on net0) -> ethernet cable ->
+#   NextVOD
+#
+# ref:https://wiki.archlinux.org/index.php/Internet_sharing
+set -ex
+sudo sysctl net.ipv4.ip_forward=1
+net0=eth0
+internet0=usb0
+echo enable nat with iptables
+sudo iptables -t nat -A POSTROUTING -o $internet0 -j MASQUERADE
+sudo iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i $net0 -o $internet0 -j ACCEPT
+echo Done
+# vim:et sw=2 ts=2 ai nocp sta
+
